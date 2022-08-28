a55f44a15245c31e90fd53f61f0d3bd8ae9f2ec4
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-08-28 06:01:59 +0600

    JSON API

---
+++ js/fetch.js
@@ -0,0 +1,25 @@
+// featch Documentation Link: JSON Placeholder
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //.then(response => console.log(response))
+
+/*The callback to execute when the Promise is resolved.
+Attaches callbacks for the resolution and/or rejection of the Promise */
+
+.then(response => response.json()) // .json is not to similar JSON.parse but Close to JSON.parse
+
+.then(json => console.log(json))
+
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+fetch(url)
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+
+function getObject() {
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    fetch(url)
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
---
+++ js/json.js
@@ -0,0 +1,24 @@
+//   Javascript Object Notation is (JSON)
+// Common Propertis is -JSON.stringify, JSON.parse
+
+const shop = {
+    name: "Onlunt Shop",
+    address: {
+        state: 'Kochukhet Paikar Para',
+        city: 'Dhaka',
+        country: 'Bangladesh'
+    },
+    revinue: 123000,
+    isOpen: true,
+    isNew: false
+}
+console.log(shop);
+
+// Convert to stringify  
+const objStringify = JSON.stringify(shop);
+console.log(objStringify);
+
+//convert JSON Stringify to parse(Orginal Object)
+
+const convertStringifyToObject = JSON.parse(objStringify);
+console.log(convertStringifyToObject);
